b2cf8b1d189724f91ee19e7c51afff07bcc6e6a3
qc-kgm <52749693+qc-kgm@users.noreply.github.com>
2020-12-05 15:51:41 +0700

    Add files via upload
    add object ketquatest

---
+++ src/Object/ketquatest.java
@@ -0,0 +1,39 @@
+package Object;
+
+import java.sql.Date;
+
+public class ketquatest {
+	private String masocl;
+	private Boolean ketqua;
+	private Date thoigian;
+	private int lantest;
+	public String getMasocl() {
+		return masocl;
+	}
+	public void setMasocl(String masocl) {
+		this.masocl = masocl;
+	}
+	public Boolean getKetqua() {
+		return ketqua;
+	}
+	public void setKetqua(Boolean ketqua) {
+		this.ketqua = ketqua;
+	}
+	public Date getThoigian() {
+		return thoigian;
+	}
+	public void setThoigian(Date thoigian) {
+		this.thoigian = thoigian;
+	}
+	public int getLantest() {
+		return lantest;
+	}
+	public void setLantest(int lantest) {
+		this.lantest = lantest;
+	}
+	public ketquatest() {
+		super();
+	}
+	
+	
+}
